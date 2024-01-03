12e1422d36c1798af6e8430df73a53647c344af5
SFState <vishalsharma1907@gmail.com>
2024-01-02 22:24:21 -0800

    Updated requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='clichat',
-    version='1.0.0',
+    version='1.1.1',
     author='Vishal Sharma',
     author_email='vishalsharma1907@gmail.com',
     license='MIT',
